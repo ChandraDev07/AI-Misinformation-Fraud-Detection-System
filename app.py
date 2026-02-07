ced1bf4ff0e516665bedc8e10ba5dcd86f4ffee0
ChandraDev07 <chandrashekhar0733@gmail.com>
2026-02-07 13:29:57 +0530

    Dynamic heading based on selected module

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ menu = st.sidebar.selectbox("Choose Module", [
 
 # ================= FAKE NEWS =================
 if menu == "Fake News":
-    st.title("📰 Fake News Analyser Assistant")
+    st.title("📰 Fake News Verification Assistant")
     model = joblib.load("fake_news_module/fake_news_model.pkl")
     vectorizer = joblib.load("fake_news_module/vectorizer.pkl")
 
@@ -78,7 +78,7 @@ elif menu == "Claim Verification":
 
 # ================= FAKE PROFILE =================
 elif menu == "Fake Profile":
-    st.title("🙎 Fake Profile Assistant")
+    st.title("🙎 Fake Profile Verification Assistant")
     model = joblib.load("profile_module/profile_model.pkl")
 
     followers = st.number_input("Followers")
